9c3423758ab48d206bf9f6834918545ec5a33cc8
luciferL <luciferlikeme@163.com>
2018-09-20 09:04:23 +0800

    class

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -7,6 +7,7 @@ import Login from './Component/Login/Login';
 import Home from './Component/Home/Home';
 import './Math/Config'
 
+
 ReactDOM.render(
         <BrowserRouter basename='/'>
             <Switch>
